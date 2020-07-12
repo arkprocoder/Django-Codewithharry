744b75f6c4255b7bbd23bb3e676f846e2d486491
Jay Patel <jay.p1@ahduni.edu.in>
2020-07-12 12:33:03 +0530

    All Textutils Added

--- textutils/urls.py
+++ textutils/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('analyze', views.analyze, name='analyze'),
+    path('ex1', views.ex1, name = 'ex1'),
     # path('capitalizeFirst', views.capfirst, name='capfirst'),
     # path('newlineremove', views.newlineremove, name='newlineremove'),
     # path('spaceremove', views.spaceremove, name='spaceremove'),
--- textutils/views.py
+++ textutils/views.py
@@ -11,25 +11,74 @@ def index(request):
 # def about(request):
 #     return HttpResponse("About Jay Patel")
 
+def ex1(request):
+    return HttpResponse(''' 
+    <h2>Navbar</h2> 
+    <li><a href = "">Google</a></li>
+    <li><a href = "">Youtube</a></li>
+    <li><a href = "">Twitter</a></li>
+    <li><a href = "">Facebook</a></li>
+    <li><a href = "">Instagram</a></li>''')
+
+
+
 def analyze(request):
     #Get the text
     djtext = request.GET.get('text', 'default')
-    removepunc = request.GET.get('removepunc', 'off') 
-    print(djtext)
-    print(removepunc)
-    #Analyze the text
+    #check the check box value
+    removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+    charcount = request.GET.get('countchar', 'off') 
 
+    #Analyze the text
     if removepunc=="on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
+
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    elif fullcaps == "on":
+        analyzed = ""
+        for char in djtext:
+            analyzed += char.upper()
+        params = {'purpose': 'Full Capitalise', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif newlineremover == "on":
+        analyzed = ""
+        for char in djtext:
+            if char!="\n":
+                analyzed += char
+        params = {'purpose': 'New Line Remover', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    
+    elif extraspaceremover == "on":
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1] == " "):
+                analyzed += char
+
+        params = {'purpose': 'Extra Space remover', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif charcount == "on":
+        count = 0
+        for index, char in enumerate(djtext):
+              if not(djtext[index] == " "):
+                count = count + 1
+
+        analyzed = "Characters in your Sentence are : {}".format(count)
+        params = {'purpose' : 'Character Count', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
     else:
         return HttpResponse("Error")
 
-    params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-    return render(request, 'analyze.html', params)
 
 # def capfirst(request):
 #     return HttpResponse("""capitalize first <br> <a href = "http://127.0.0.1:8000/" >Home</a>""")
